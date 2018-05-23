3ee3adbc43767dd5b5e3637435b9b7032ffe6385
Melody Hawkins <melodyhawkins@gmail.com>
2018-05-22 17:52:47 -0700

    completed takehome1

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
 var myName = "Melody";
 console.log(myName);
+var classGoal = "to learn JavaScript so I can become a front-end developer.";
+console.log(classGoal);
+var myFavoriteHobby = "reading";
+console.log(myFavoriteHobby);
