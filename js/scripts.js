b78780fca1942658bc38e62533fd8cd176abeffc
Melody Hawkins <melodyhawkins@gmail.com>
2018-05-24 11:26:17 -0700

    day2

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var myName = "Melody";
+let birthday = new Date('10.6.76');
+const myGoal = "to learn JavaScript so I can become a front-end developer for Bitwise Industries, Inc.";
+console.log(myName, birthday, myGoal);
+console.log(Date.UTC(birthday));
+let chores = ['feed the dog', 'try not to kill the cat', 34, [1, 4, 5]];
+alert(chores);
+let userAnswer = prompt ("Did you brush your teeth today?", "Yes");
+console.log(userAnswer);
+
+let favoriteMovie = "Gone With the Wind";
+let favoriteFood = "tacos";
+console.log(favoriteMovie, favoriteFood);
+const yearMovieCameOut = new Date(1936);
+const ingredients = ['corn tortillas', 'carne asada', 'cilantro', 'onions', 'cabbage', 'lime', 'green salsa'];
+console.log(yearMovieCameOut, ingredients);
+alert(favoriteFood + ingredients);
+confirm(favoriteMovie, yearMovieCameOut);
