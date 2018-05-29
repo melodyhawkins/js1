54f8a9251837fa916b2f2acb856690d47c15e818
Melody Hawkins <melodyhawkins@gmail.com>
2018-05-29 16:35:47 -0700

    revised completed takehome-day3

--- js/scripts.js
+++ js/scripts.js
@@ -4,38 +4,38 @@
 // Prompts your user for their name.
 // Next, ask the user's favorite color. (Provide the user with the basic 10 colors as options i.e. red, orange, yellow, green, blue, pink, purple, black, white, and grey).
 // Depending on the color, write 9 different responses for the possible answers. Include the user's name in the response message.
-let firstName = prompt("What is your first name?", "Ryeker");
+let firstName = prompt("What is your first name?");
 let favoriteColor = prompt("What is your favorite color from the following list?", "red,"+ ' ' + "orange," + ' ' + "yellow,"+' ' + "green,"+ ' ' + "blue,"+ ' ' + "pink,"+ ' ' + "purple,"+ ' ' + "black,"+ ' ' + "white,"+ ' ' + "grey");
 switch (favoriteColor) {
   case 'white':
-    alert('Ryeker, your choice is very bright.');
+    alert(firstName + ' '+ 'your choice is very bright.');
     break;
   case 'black':
-    alert('Ryeker, your choice is very bold.');
+    alert(firstName + ' '+ 'your choice is very bold.');
     break;
   case 'grey':
-    alert('Ryeker, your choice is very bland.');
+    alert(firstName + ' '+ 'your choice is very bland.');
     break;
   case 'pink':
-    alert('Ryeker, you have excellent taste in colors!');
+    alert(firstName + ' '+ 'you have excellent taste in colors!');
     break;
   case 'red':
-    alert('Ryeker, you must like strawberries.');
+    alert(firstName + ' '+ 'you must like strawberries.');
     break;
   case 'orange':
-    alert('Ryeker, you must like halos.');
+    alert(firstName + ' '+ 'you must like halos.');
     break;
   case 'yellow':
-    alert('Ryeker, you must like bananas.');
+    alert(firstName + ' '+ 'you must like bananas.');
     break;
   case 'purple':
-    alert('Ryeker, you must like grapes.');
+    alert(firstName + ' '+ 'you must like grapes.');
     break;
   case 'green':
-    alert('Ryeker, you must like trees.')
+    alert(firstName + ' '+ 'you must like trees.');
     break;
   case 'blue':
-    alert('Ryeker, you must like the sea.')
+    alert(firstName + ' '+ 'you must like the sea.');
     break;
   default:
 
