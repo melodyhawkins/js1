f4d489eb8507a05104359fb145c6771f128a2e57
Melody Hawkins <melodyhawkins@gmail.com>
2018-06-03 16:05:45 -0700

    day-4 in-class

--- js/scripts.js
+++ js/scripts.js
@@ -1,198 +1,130 @@
-// Day 3
-// const name ="Melody"; // truthy or true
-// const age = null; //falsey or false
-
-// if (name || age) {
-  // alert('one exists');
-// }
-// Josh's Notes
-//THE LOGICAL OPERATORS
-// Logical AND ( && ) // Both have to be ture
- // let name = "joshua";
- // let age = 18;
-
- // if(name && age){
-   // alert('both exist');
- // }
-
-// Logical OR ( || ) // One or the other must be true //js will stop on the the first true one
- // let name = "joshua";
- // let age = 18;
-
- // if(name || age){
-   // alert('both exist');
- // }
-// Logical NOT ( ! ) //Checks if it is false if so its true
- // let name = "";
- // let age = 18;
-
- // if(!name){
-   // alert('one exist');
- // }
-
- // if(!name && age){
-   // alert('both exist');
- // }
-// Truthy https://developer.mozilla.org/en-US/docs/Glossary/truthy //In JavaScript, a truthy value is a value that is considered  true when evaluated in a Boolean context
-// Falsy https://developer.mozilla.org/en-US/docs/Glossary/falsy //A falsy value is a value that translates to false when evaluated in a Boolean context.
- // let name = ""; //Falsy
- // let age = 18; //Truthy
-
- // if(name || age){  //Truthy
-    // alert('both exist');
-  // }
-// let name = '';
-// let age = 32;
-
-// if(!name && age){
-  // alert('one exists');
-// }
-// comparison operators //Loose equal ( == ) //Strict equal ( === )
-   const age = 18;//number
-   const stringAge = '18';//string
-//End Josh's notes
-   if(age == stringAge) {
-     // alert('they\'re the same!');
-   }
-// if you forget and do this, you are setting, not checking:
-if(numAge = stringAge) {
-  // alert('they\'re the same!');
+// Day 4 Class Notes
+let inside = true;
+if(inside){
+  console.log('i am inside')
+}else{
+  console.log('i am not inside')
 }
-// True
-if(numAge !== stringAge) {
-  // alert('they\'re the same!');
-}
-// false
-if(numAge != stringAge) {
-  // alert('they\'re the same!')
-}
+//Ternaries
+//condition ? expression1 : expression2
 
-let objectOne = {
-  name: "Melody"
-};
+let example2 = inside? "I am inside" : 'i am not inside'
+//console.log(example2)
 
-let objectTwo = {
-  name: "Melody"
-};
+// let number = 10
+// let num = (number > 20) ? "number is greater than 20" : "number is less than 20"
+// console.log((number > 20) ? "number is greater than 20" : "number is less than 20")
 
-// alert(objectOne == objectTwo); //ALWAYS FALSE - TWO OBJECTS WILL NEVER BE EQUAL TO EACHOTHER
-// turns out to be false. JS saves everything that it has, except for objects, by reference. With an object, JS carves out a piece of memory, calls it object, and creates a pointer and stores it there, so two objects are never equal to eachother ever because they are never saved in the same place. two objects with the exact same value are saved in different places in memory so will never see two objects as equal to eachother.
-// if(numAge > stringAge.length) {
-  // alert('whaaaaaaaat?');
+// let number = 15;
+// console.log(number > 10 ? "number is greater" : "number is less than")
+
+let name = false;
+let email = true;
+//
+// if(name){
+//   console.log("I have name")
+// }
+// else if (email) {
+//   console.log("I have email")
+// }
+// else{
+//   console.log("i have none")
 // }
 
-// var myString = 'melody, maui, happy';
-// var objectString= new String ('melody, maui, happy');
+// as a Ternary
+name ? "i have name" : email ? "i have email" : "i have none"
+console.log(name ? "i have name" : email ? "i have email" : "i have none")
 
-// if(myString <= objectString){
-  // alert('they\'re the same');
-// }
+let imAmHUngry = true;
+let money = 5;
+// Ternaries [only use when either one can be one or the other (if else statements - otherwise use switch statements)]
+let hungry = imAmHUngry ? ((money >10)? "i am hungry and i have more than 10" : "i am hungry and i have less than 10") :
+             ((money> 10)? "i am not hungry and i have more than 10" : "i am not hungry and i have less than 10")
 
-// incremenet
-let limit = 12;
-console.log(limit);
-limit++; //same as limit = limit + 1;
-console.log(limit);
-limit + 1;
-console.log(limit);
-limit += 7; //same as limit = limit + 7;
-console.log(limit);
-const upperlimit = 23;
-
-// NaN - not a number - value that something can be if it is not a number. Example:
-// let myNumber = "Ted";
-// console.log(myNumber++);
-
-// Way to check if something is NaN
-// let myNumber = "cheese";
-
-// isNaN(myNumber);
-
-// console.log(isNaN(myNumber++)); //true
-
-// let myNumber = 77;
-// console.log(!isNaN(myNumber++)); //true b/c 77 is not not a number
-
-// let myNumber = 77;
-// myNumber = parseInt(myNumber;)
-// console.log(!isNaN(myNumber++)); //true b/c 77 is not not a number
-
-// let userAnswerFn = prompt("What is your first number?" , '10');
-// let userAnswerSn = prompt("What is your second number?", '10');
-// userAnswerFn = parseInt(userAnswerFn);
-// userAnswerSn = parseInt(userAnswerSn);
-// alert(userAnswerFn + userAnswerSn);
-
-// let numOne =  parseInt(prompt('Give me a number' , '10'));
-// let numTwo = parseInt(prompt('Give me a number', '10'));
-// alert(numOne + numTwo);
-
-// if else statements
-// if(numOne > numTwo) {
-  // alert("Your first number is bigger than your second number");
-// } else if(numOne === numTwo) {
-  // alert("They're the same!");
-// }else if(numOne < numTwo){
-  // alert("Number one is smaller than number Two!!")
+console.log(hungry);
+// if (imAmHUngry){
+//   if (money > 10){
+//       'i am hungry and i have more than 10'
+//  }
 // }else {
-  // alert('Something went wrong...');
+//   if (money >10){
+//     'i am not hungry and i have more than 10'
+//   }
+//   else {
+//     'i am not hungry and i have less than 10'
+//   }
 // }
+let IamWearingYellow = false
+// IamWearingYellow ? "yes i am wearing yellow" : "no i am not";
 
-// if(numOne > numTwo) {
-  // alert('yay');
-// }else{
-  // alert('booo');
-// }
+let number = 23;
+let num = (number = 23 && IamWearingYellow) ? "number = 23 and yellow" : "number not 23 and not yellow"
+console.log(num); //do not have to write out number because defined entire declaration as "num"
 
-// Switch Statements : basically the same concept as an if else, but a little bit cleaner and may want to use over it in some conditions.
-// try to use as few "if else" statements as possible, use switch statements instead.
-let firstName = prompt ("what is your first name?", "Tom");
-switch (firstName) {
-  case 'tom':
-    alert("Hello Tom!");
-    break; //have to include this otherwise JS will continue to evaluate code.
-  case 'john':
-  case 'harry':
-  case 'peter':
-    alert('Your name is very common.');
-    break;
-  default:
-    alert('I have never heard that name before.');
-    break;
-}
+// Arrays
+// They are a really good way of saving data. If you have a website and you are building a online store for a client. your job would be to get all the data conntected to the products from the backend sent to you in an array and you would look in that array for a picture, price, title, description for every product and display it on the screen. way of transporting and accessing data. Array type object [evrything in JS is an object]
+// [] (most basic form of an array) Arrays give you access to length - tells you how many things are in an array, kind of like a string, gives you the count of the things inside. It index - a numerical reference that gives you a reference to a thing inside of an array - known as zero indexed (0 is always the first number in an array). the way to access - name the data (variable); can have an array inside of an array, can have a boolean inside of an array. The length of the array is still how many members are in the array.
+// let arr = ["javascript", 100, {}, [], true];
+// console.log(arr[4]);
+// console.log(arr.length);
 
-let firstName = prompt ("what is your first name?", "Tom");
-switch (firstName.toLowerCase()) { //it will always be a lowercase value that comes back.
-  case 'tom':
-    alert("Hello Tom!");
-    break; //have to include this otherwise JS will continue to evaluate code.
-  case 'john':
-  case 'harry':
-  case 'peter':
-    alert('Your name is very common.');
-    break;
-  default:
-    alert('I have never heard that name before.');
-    break;
-}
-// Day One
-var myName = "Melody";
-let birthday = new Date('10.6.76');
-const myGoal = "to learn JavaScript so I can become a front-end developer for Bitwise Industries, Inc.";
-console.log(myName, birthday, myGoal);
-console.log(Date.UTC(birthday));
-let chores = ['feed the dog', 'try not to kill the cat', 34, [1, 4, 5]];
-alert(chores);
-let userAnswer = prompt ("Did you brush your teeth today?", "Yes");
-console.log(userAnswer);
+// Try it out #1
+let arr = ['The Post', 21, true, 1994];
+// console.log(arr.length);
 
-// Day Two
-let favoriteMovie = "Gone With the Wind";
-let favoriteFood = "tacos";
-console.log(favoriteMovie, favoriteFood);
-const yearMovieCameOut = new Date(1936);
-const ingredients = ['corn tortillas', 'carne asada', 'cilantro', 'onions', 'cabbage', 'lime', 'green salsa'];
-console.log(yearMovieCameOut, ingredients);
-alert(favoriteFood + ingredients);
-confirm(favoriteMovie, yearMovieCameOut);
-console.log(myName);
+// console.log(Array.isArray(arr));
+// console.log(arr);
+// arr.push("Pizza");
+// console.log(arr); //added pizza to the end of the array.
+// control flow: check whether it is an array first and then if it is move forward and if not then make it one.
+// push - method attached to the array object.
+// unshift - add at beginning of the array
+
+// console.log(arr);
+// // arr.unshift("Pizza");
+// const removed = arr.pop();
+// console.log(arr); //added pizza to the end of the array.
+//
+// let first = arr.shift();//remove the first item in the array AND returns the returned item to you as your variable 'first'
+// console.log(arr, first);
+// console.log(arr[arr.length-1]); // access/how to find the last number/index in the array
+// console.log(arr[arr.length-2]); //access/how to find the second to last number/index in the array
+
+// Pop -can pop something off the end of an Array - ONLY FOR THE LAST ITEM
+// Splice - allows you optionally to do multiple things at the same time. It has multiple different arguments that it can take. The information in square brackets inside parentheses are optional. Parameters = arguments => thing you are putting inside the parentheses. To use splice in its most basic way we can use one parameter:
+// arr.splice(2);//one argument will remove that index and everything after.
+console.log(arr, 'before');
+let removedItems = arr.splice(2, 1, false, "Rocky"); // 1 is how many things you want to delete [the delete count]. with 2 arguments you start removing items at the first argument (index) and then remove only the number of items you passed in as the second argument (delete count). Should remove 1 thing, remove that one thing with "false" and then follow it up with the additional objects.
+console.log(arr, "this is the result of splice");
+console.log(removedItems);
+
+// Slice = basically just makes a copy. does not do anything to the original at all.
+// indexOf = use this to find where it is in the index and then to slice it out of the array. Will only give you the FIRST index.
+console.log(arr.indexOf("The Post"));
+// lastIndexOf = only gives you the LAST index.
+// reverse = reshuffles the array so the first becomes the last, etc.
+arr.reverse();
+console.log(arr);
+
+// Multi-dimensional arrays - arrays within Arrays- how can we access a certain member inside of an array?
+const products = [["blanket", [12.99, 10.99], "The best blanket around."], ['rattle', 4.99, "It rattles man..."], ['diapers', 12.99, 'They catch the doo doo']];
+alert(products[0][1][1]);
+
+// Try it out #2
+// In your scripts.js file, create 5 arrays, each with one of your top 5 movies' title and the main character's name
+// Add all of these arrays to a final array named favoriteMovies
+// Create an array with just the titles of your favorite movies and alert it
+
+let favMov1 = ["Girls Just Want to Have Fun", "Sarah Jessica Parker"];
+let favMov2 = ["The Notebook", "Ryan Gosling"];
+let favMov3 = ["Wall Street", "Charlie Sheen"];
+let favMov4 = ["Legally Blonde", "Reese Witherspoon"];
+let favMov5 = ["Any Given Sunday", "Cameron Diaz"];
+
+let favoriteMovies = [];
+favoriteMovies.push(favMov1, favMov2, favMov3, favMov4, favMov4, favMov5);
+
+let titles =[];
+titles.push(favoriteMovies[0][0], favoriteMovies[1][0], favoriteMovies[2][0], favoriteMovies[3][0], favoriteMovies[4][0], favoriteMovies[5][0]);
+
+alert(titles);
