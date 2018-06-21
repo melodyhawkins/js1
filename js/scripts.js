5e49aae2aec0008a022d0144a1b7686fcdb2b66b
Melody Hawkins <melodyhawkins@gmail.com>
2018-06-20 17:26:23 -0700

    day-7

--- js/scripts.js
+++ js/scripts.js
@@ -1,104 +1,77 @@
-// Day 5 -Functions
-// So far we have written code in the global scope, for the most part, functions allow you to take a piece of code that you can pass around. Place in {} - it is an entire block of code that you can execute on any type of data that you want. Take a piece of code, set it aside and call it as many times as you want.
-// Rules: They can (optional) take in data (input); they can (optional) feed data back to you (output); console.log is a function. One of the hallmarks of a function is parentheses.
-// Examples:
-console.log("Hello World!");
-// array.pop(); //- function takes in zero arguments and return value is the member of the array value you removed.
-// before you start your final project you should data frame your project. what methods, how to use them, when called, when user going to get involved, etc. Always have a plan.
-// Anonymous and Name functions
-// functions are what lets you make your code dynamic
-// functions are a JS keyword - b/c expects a certain thing to be true when it sees function.
-// Example, we want to take a string and capitalize the first letter in each word of the string:
-// function capitalCase(string) { //function definition. //the arguments located in the () in a function defintion have no value until you call the function.
-  // return string.toUpperCase(); //nothing has been called yet. reutrn value of capitalCase
-// };
-function capitalCase(string) {
-   let words = string.split(' '); //split lets you take any character and split it into separate arrays.
-   words.forEach(word => {
-      let letters = word.split('');
-      let capped = letters[0].toUpperCase() //word is the indivdual loop we are on in our forEach loop; we can use indexOf
-      letters.splice(0, 1, capped);
-   });
-   return words.join(' '); //join that array back together by a space - join takes an array and puts it back into a string.
-};
-
-// let newString = capitalCase("peter piper"); //function call - this is where (string) gets defined.
-// console.log(newString);
-
-// function(stuff) { //Anonymous function -  most likely part of a call-back or part of something else. Does not have a name, still follows all the rules.
-  // console.log(stuff)
-// };
-
-// function expression takes an Anonymous expression and assigns it to a variable. hoisting only works with function declarations NOT function expressions.
-
-let capsName = capitalCase('captain america');
-
-// myCoolFunction("cheese");
-
-// function myCoolFunction(stuff) {
-  // console.log(stuff)
-// };
-
-// Arrow Functions - new to ES6 -learn about in class.
-// let myCoolFunction = () => { // SIMPLEST fat arrow function
-//   console.log(stuff)
-// };
-// let myCoolFunction = (stuff) => { //fat arrow function
-//   console.log(stuff)
-// };
-// // IF FAT ARROW FUNCTION ONLY TAKES ONE ARGUMENT CAN WRITE WITHOUT parentheses:
-// let myCoolFunction = stuff => { // 1 argument fat arrow function
-//   console.log(stuff)
-// };
+// // Day 7
+// //Try it out #2
+// let button = document.querySelector('button');
+// let div = document.querySelector('div');
+// // const body = document.querySelector('body');
 //
-// let myCoolFunction = (stuff, things, etc) => { // multiple argument fat arrow function
-//   console.log(stuff)
-// };
-
-// The order of the arguments in functions matters.
-
-// function divide(arg1, arg2) {
-//   return arg2/arg1;
-// }
-// Try it out #1
-// On your day5 branch, write a function that calculates a dog's age. It should:
-// Accept 1 argument (age of the dog in years).
-// Return the age of the dog using the conversion rate of 1 year to 7 "dog" years.
-// Alert the answer.
-// function ageDog(years) {
-//   return years * 7;
-// };
+// button.addEventListener('click', e =>{
+//   e.target.textContent = "Goodbye cats!!";
+// });
 //
-// alert(ageDog(parseInt(prompt("How old is your dog in years?", "3"))));
+// //console.log(button.textContent);// another way to check text content.
+//
+// //innerHTML - use textContent instead if you have the choice
+//  div.innerHTML = "<p>lorem ipsum dolores cantu!</p>";
+//
+// //Creating Elements
+//
+// //.createElement()
+// // let element = document.createElement(tagName[, options]);
+//
+// let newH1 = document.createElement('h1');
+// newH1.textContent = "Login Here!!"
+// console.log(newH1);
+//
+// // append child - how you get JS to HTML file. Becomes the last element of our existing HTML
+// // body.appendChild(newH1);
+//
+// //how to add before the body
+// // parentelement.insertBefore(new node, reference node)
+// newH1.className = "text-center";
+// // body.insertBefore(newH1, div);
+//
+// //.id
+// div.id = "red_text";
+//
+// //.notation - how you connect a method or a property to an object (e.g. method.object)
+//
+// //classList
+// //is itself a method - has multiple arguments you can give it.
+// //add a list of classes that are already on the element:
+// newH1.classList.add("text-center");
+//
+// div.addEventListener('click', e =>{
+//   div.classList.toggle('text-center');
+//   div.classList.replace('blue-background', 'green-background')
+// });
+// //.toggle is very useful.
+// console.log(div.classList.contains('text-center'));
+//
+// //.setAttribute() - can set the attribute to anything else.
+//
+// myImg = document.createElement('img');
+// //set the src attribute:
+// myImg.setAttribute('src', 'https://images.unsplash.com/photo-1506184719731-01e394588102?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6295b9fe1c5af6125d746c763368adbd&auto=format&fit=crop&w=991&q=80');
+// //add the image to the page:
+// div.appendChild(myImg);
+// console.log(myImg)
 
-// Now, write a function that tells you what a "lifetime supply" will be for any product. It should:
+// Fizz Buzz Test
+//check the modulus to determine if it is a multiple of 3 or 5
+//createElement - b/c need it to print to the page need an Element
 //
-// Accept 2 arguments (age of person now, amount of product per day).
-// Calculate the supply needed for the rest of the user's life, based on a constant max age of 80.
-// Alert the answer in this format: "You will need x to last you until you're y.
-// BONUS: write it to accept a dynamic max age.
+let body = document.querySelector('body');
 
-// // let
-// if(!isNaN(years)){
-//   return years * 7;
-// } else {
-//   ageDog(parseInt(prompt("How old is your dog in years?", "3"))));
-// }
-
-// function lifetimeCalculator(age, dailyUse) {
-//   if(!isNaN(age) && !isNaN(dailyUse)) {
-//     const death = 80;
-//     return (death - age) * (dailyUse * 365);
-//   } else {
-//     userAge = prompt('how old are you?', '41');
-//     userUse = prompt("How much do you use every day?");
-//     lifetimeCalculator(parseInt(userAge), parseInt(userUse));
-//   }
-// };
-//
-// let userAge = prompt('how old are you?', '41');
-// let userUse = prompt("How much do you use every day?", '1');
-//
-// let usage = lifetimeCalculator(parseInt(userAge), parseInt(userUse));
-//
-// alert(`You will need ${usage} to last until you're 80.`);
+for (let i = 1; i <= 100; i++) {
+  let p = document.createElement('p');
+  if (i % 3 === 0 && i % 5 === 0) {
+    p.textContent = 'FizzBuzz';
+  }else if (i % 5 === 0) {
+		p.textContent = ('Buzz');
+	}else	if (i % 3 === 0) {
+		p.textContent = ('Fizz');
+	}else {
+    p.textContent = i;
+  }
+  body.appendChild(p);
+}
